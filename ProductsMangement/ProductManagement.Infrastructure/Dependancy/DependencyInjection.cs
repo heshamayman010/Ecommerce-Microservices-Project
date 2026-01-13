ee5236052c103a9f94ab7383c80942062cd779c8
hesham ayman <hesham.ayman@easacc.com>
2026-01-13 02:33:09 +0200

    befor docker compose

--- ProductsMangement/ProductManagement.Infrastructure/Dependancy/DependencyInjection.cs
+++ ProductsMangement/ProductManagement.Infrastructure/Dependancy/DependencyInjection.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProductManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace UserManagement.Infrastructure
 {
@@ -10,8 +13,14 @@ public static class DependencyInjection
   public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
   {
 
+    string mymainconnectionstring=configuration.GetConnectionString("DefaultConnection");
+   string NewConnectionUsingEnviroment= mymainconnectionstring
+   .Replace("$MYSQL_HOST",Environment.GetEnvironmentVariable("MYSQL_HOST")).
+   Replace("$MYSQL_PASSWORD",Environment.GetEnvironmentVariable("MYSQL_PASSWORD")).
+   Replace("$PortNumber",Environment.GetEnvironmentVariable("PortNumber"));
+
     services.AddDbContext<DataContext>(options => {
-      options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+      options.UseMySQL(NewConnectionUsingEnviroment);
     });
 
     services.AddScoped<IProductsRepository, ProductsRepository>();
@@ -19,3 +28,11 @@ public static class DependencyInjection
   }
 }
 }
+
+
+
+    // string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+    // string connectionString = connectionStringTemplate
+    //   .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+    //   .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+
