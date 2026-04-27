b966b29ccd11a6d1aaa70ead836c066ab0e42392
Heshamayman00 <hesham.ayman.abdelsamee@gmail.com>
2026-04-28 00:26:41 +0300

    After Docker Compose For Product And Users

--- FrontEnd/src/environment.ts
+++ FrontEnd/src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7053/api/auth/', 
-    productsMicroserviceUrl: 'https://localhost:7228/api/products/'
+    apiUrl: 'http://localhost:9090/api/auth/', 
+    productsMicroserviceUrl: 'http://localhost:8080/api/products/'
   };
   
